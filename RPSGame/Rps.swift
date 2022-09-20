b3a4a0ca04c352de3e69022d6aaf27003a9db3f8
hooni <jhlee909090@gmail.com>
2022-09-21 01:27:10 +0900

    Rps

---
+++ RPSGame/Rps.swift
@@ -0,0 +1,11 @@
+
+import Foundation
+import UIKit
+
+
+enum Rps: Int { //원시값을 설정하면 순서(바위가 0번쨰 --- 가 생김)
+
+    case rock 
+    case paper
+    case scissors
+}
--- RPSGame/ViewController.swift
+++ RPSGame/ViewController.swift
@@ -9,11 +9,76 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var comImage: UIImageView!
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var comChoiceLabel: UILabel!
+    @IBOutlet weak var myChoiceLabel: UILabel!
+    
+    var myChoice: Rps = Rps.rock
+    
+    var comChoice: Rps = Rps(rawValue: Int.random(in: 0...2))!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        comImage.image = #imageLiteral(resourceName: "ready")
+        myImageView.image = UIImage(named: "ready.png")
+        
+        comChoiceLabel.text = "준비"
+        myChoiceLabel.text = "준비"
+         
     }
 
+    @IBAction func rpsBtnTapped(_ sender: UIButton) {
+   //가위바위보를 선택해서 그 정보를 저장해야함
+//
+//        guard let title = sender.currentTitle
+//
+//        else{
+//            return
+//        }
+//
+        
+        let title = sender.currentTitle
+        print(title)
+        
+        
+        switch title {
+        case "가위" :
+           myChoice = Rps.scissors
+        case "바위" :
+            myChoice = Rps.rock
+        case "보" :
+            myChoice = Rps.paper
+        default: //문자열이라서 모든 경우가 들어가는게 아니라서 디포릍 작성
+            break
+        }
+        
+        
+    }
+    
+    
+    @IBAction func selectBtnTapped(_ sender: UIButton) {
+        
+        switch comChoice {
+        case Rps.rock:
+            comImage.image = #imageLiteral(resourceName: "ready")
+            comChoiceLabel.text = "바위"
+        case Rps.paper:
+            comImage.image = #imageLiteral(resourceName: "paper")
+            comChoiceLabel.text = "보"
 
+        case Rps.scissors:
+            comImage.image = #imageLiteral(resourceName: "scissors")
+            comChoiceLabel.text = "가위"
+        }
+    }
+    
+    @IBAction func resetBtnTapped(_ sender: UIButton) {
+    }
+    
+    
 }
 
