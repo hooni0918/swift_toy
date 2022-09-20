c87703c653a7667d2665f073005ca847c816885a
hooni <jhlee909090@gmail.com>
2022-09-20 15:33:43 +0900

    랜덤 주사위 게임

--- DiceGame/ViewController.swift
+++ DiceGame/ViewController.swift
@@ -1,19 +1,31 @@
-//
-//  ViewController.swift
-//  DiceGame
-//
-//  Created by 이지훈 on 2022/09/17.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var firstImageView: UIImageView!
+    @IBOutlet weak var secondImageVIew: UIImageView!
+    
+
+    var DiceArray: [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2") , #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5") , #imageLiteral(resourceName: "black6")]
+    //#imageLiteral( 을 사용하였습니다. Assets에 미리 담아두었습니다.
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollbtntapped(_ sender: UIButton) {
+        //첫번째 이미지뷰의 이미지를 랜덤으로 변경
+        firstImageView.image = DiceArray.randomElement()
+        
+        //두번째 이미지뷰의 이미지를 랜덤으로 변경
+        secondImageVIew.image = DiceArray.randomElement()
+        
+    
+    
+    }
+    
 }
 
