1f584bac2bbee824ff785d0d23749f62e3aa6460
YIZIHN <109647045+hooni0918@users.noreply.github.com>
2022-12-05 18:07:42 +0900

    화면이동 및 데이터 전달

---
+++ 화면이동 및 데이터 넘기기/NextVC/MyViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MyViewController.swift
+//  NextVC
+//
+//  Created by 이지훈 on 2022/12/05.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+
+}
